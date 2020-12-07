94a3cd50581bdeb9fd00cafc229529a7ca81642a
yuxiRen <rita.ren.yuxi@gmail.com>
2020-12-06 23:30:18 -0800

    UserControllerTest

--- src/test/java/com/example/demo/controllers/UserControllerTest.java
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
@@ -21,14 +23,15 @@ class UserControllerTest {
 
     private UserController userController;
 
+    private User user;
+    private Optional<User> userOptional;
+
     private UserRepository userRepository = mock(UserRepository.class);
 
     private CartRepository cartRepository = mock(CartRepository.class);
 
     private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
-    private CreateUserRequest createUserRequest;
-    private ResponseEntity<User> userResponseEntity;
-    private User user;
+
     @BeforeAll
     static void beforeAll() {
     }
@@ -39,36 +42,56 @@ class UserControllerTest {
         TestUtils.injectObject(userController, "userRepository", userRepository);
         TestUtils.injectObject(userController, "cartRepository", cartRepository);
         TestUtils.injectObject(userController, "bCryptPasswordEncoder", encoder);
+        user = new User();
+        user.setId(1L);
+        user.setUsername("Test");
+        user.setPassword("test");
+        userOptional = Optional.of(user);
     }
 
     @Test
     public void createUserHappyPath() throws Exception {
         when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
-        createUserRequest = new CreateUserRequest();
-        userResponseEntity = userController.createUser(createUserRequest);
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+
         createUserRequest.setUsername("test");
         createUserRequest.setPassword("testPassword");
         createUserRequest.setConfirmPassword("testPassword");
+        ResponseEntity<User> userResponseEntity = userController.createUser(createUserRequest);
         assertNotNull(userResponseEntity);
         assertEquals(200, userResponseEntity.getStatusCodeValue());
 
-        user = userResponseEntity.getBody();
-        assertNotNull(user);
-        assertEquals(0, user.getId());
-        assertEquals("test", user.getUsername());
-        assertEquals("thisIsHashed", user.getPassword());
+        User newUser = userResponseEntity.getBody();
+        assertNotNull(newUser);
+        assertEquals(0, newUser.getId());
+        assertEquals("test", newUser.getUsername());
+        assertEquals("thisIsHashed", newUser.getPassword());
     }
 
     @Test
     void findById() {
-        assertEquals(user, userRepository.findById(0L));
+        when(userRepository.findById(1L)).thenReturn(userOptional);
+        ResponseEntity<User> userResponseEntity = userController.findById(1L);
+        assertNotNull(userResponseEntity);
+        assertEquals(200, userResponseEntity.getStatusCodeValue());
+        User returnUser = userResponseEntity.getBody();
+        assertNotNull(returnUser);
+        assertEquals(1L, returnUser.getId());
+        assertEquals("Test", returnUser.getUsername());
+        assertEquals("test", returnUser.getPassword());
     }
 
     @Test
     void findByUserName() {
+        when(userRepository.findByUsername("Test")).thenReturn(user);
+        ResponseEntity<User> userResponseEntity = userController.findByUserName("Test");
+        assertNotNull(userResponseEntity);
+        assertEquals(200, userResponseEntity.getStatusCodeValue());
+        User returnUser = userResponseEntity.getBody();
+        assertNotNull(returnUser);
+        assertEquals(1L, returnUser.getId());
+        assertEquals("Test", returnUser.getUsername());
+        assertEquals("test", returnUser.getPassword());
     }
 
-    @Test
-    void createUser() {
-    }
 }
